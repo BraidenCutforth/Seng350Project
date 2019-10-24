75c2628ec56b2ab55061b61429e97fa09181f4f8
Jordan Benning <Jordan@w134-87-187-008.wireless.uvic.ca>
2019-10-24 12:42:24 -0700

    added edit button

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -55,7 +55,8 @@ export class ProfileRoute extends BaseRoute {
     //      3. If user id == profile id then display edit profile button
     //      4. Edit button triggers function that changes elements on page 
     //      5. Submit function 
-
+    const query = req.query;
+    console.log(query);
 
     //set message
     let options: Object = {
@@ -68,7 +69,8 @@ export class ProfileRoute extends BaseRoute {
       "profilePic": "https://pbs.twimg.com/profile_images/1163541386785746944/A1nz8DcJ_400x400.jpg",
       "bio": "Business man. Brothers to Harv, Linus, Mick Donald, Aiden",
       "location": "Moscow, Russia",
-      "joined": "October 2019"
+      "joined": "October 2019",
+      "isOwnProfile": true
     };
 
     //render template
