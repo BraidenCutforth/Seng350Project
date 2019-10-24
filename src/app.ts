f3b96c4403d08523fc449782f3aa7ffbcd59415a
braidencutforth <braiden89@gmail.com>
2019-10-24 16:28:07 -0700

    add, get and delete

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import express from "express";
@@ -9,9 +8,11 @@ import { IndexRoute } from "./routes/index";
 import { LoginRoute } from "./routes/login";
 import { SignUpRoute } from "./routes/signup";
 import { ProfileRoute } from "./routes/profile";
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI || "mongodb+srv://runaway:Y2ABndDFux4zyWzPsMxa6D4h@seng350-f19-3-6-db-6khes.mongodb.net/test?retryWrites=true&w=majority"; // Eventually set to a env var
+const uri =
+  process.env.MONGO_URI ||
+  "mongodb+srv://runaway:Y2ABndDFux4zyWzPsMxa6D4h@seng350-f19-3-6-db-6khes.mongodb.net/test?retryWrites=true&w=majority"; // Eventually set to a env var
 
 /**
  * The server.
@@ -19,105 +20,110 @@ const uri = process.env.MONGO_URI || "mongodb+srv://runaway:Y2ABndDFux4zyWzPsMxa
  * @class Server
  */
 export class Server {
+  public app: express.Application;
+  public client: MongoClient;
 
-    public app: express.Application;
-    public client: MongoClient;
+  /**
+   * Bootstrap the application.
+   *
+   * @class Server
+   * @method bootstrap
+   * @static
+   * @return {ng.auto.IInjectorService} Returns the newly created injector for this app.
+   */
+  public static bootstrap(): Server {
+    return new Server();
+  }
 
-    /**
-     * Bootstrap the application.
-     *
-     * @class Server
-     * @method bootstrap
-     * @static
-     * @return {ng.auto.IInjectorService} Returns the newly created injector for this app.
-     */
-    public static bootstrap(): Server {
-        return new Server();
-    }
+  /**
+   * Constructor.
+   *
+   * @class Server
+   * @constructor
+   */
+  constructor() {
+    //create expressjs application
+    this.app = express();
+    this.client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    this.client.connect(err => {
+      if (err) throw err;
+      console.log("Connection success");
+    });
 
-    /**
-     * Constructor.
-     *
-     * @class Server
-     * @constructor
-     */
-    constructor() {
-        //create expressjs application
-        this.app = express();
-        this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        this.client.connect(err => {
-            if (err) throw err;
-            console.log('Connection success');
-        });
+    this.initializeDb();
 
+    //configure application
+    this.config();
 
-        this.initializeDb();
+    //add routes
+    this.routes();
+  }
 
-        //configure application
-        this.config();
+  private async initializeDb() {}
 
-        //add routes
-        this.routes();
-    }
+  /**
+   * Configure application
+   *
+   * @class Server
+   * @method config
+   */
+  public config() {
+    //add static paths
+    this.app.use(express.static(path.join(__dirname, "public")));
 
-    private async initializeDb() {
+    //configure pug
+    this.app.set("views", path.join(__dirname, "views"));
+    this.app.set("view engine", "pug");
 
-    }
+    //mount logger
+    this.app.use(logger("dev"));
 
+    //mount json form parser
+    this.app.use(bodyParser.json());
 
-    /**
-     * Configure application
-     *
-     * @class Server
-     * @method config
-     */
-    public config() {
-        //add static paths
-        this.app.use(express.static(path.join(__dirname, "public")));
+    //mount query string parser
+    this.app.use(
+      bodyParser.urlencoded({
+        extended: true
+      })
+    );
 
-        //configure pug
-        this.app.set("views", path.join(__dirname, "views"));
-        this.app.set("view engine", "pug");
+    //mount cookie parser middleware
+    this.app.use(cookieParser("SECRET_GOES_HERE"));
 
-        //mount logger
-        this.app.use(logger("dev"));
+    // catch 404 and forward to error handler
+    this.app.use(function(
+      err: any,
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) {
+      err.status = 404;
+      next(err);
+    });
 
-        //mount json form parser
-        this.app.use(bodyParser.json());
+    //error handling
+    this.app.use(errorHandler());
+  }
 
-        //mount query string parser
-        this.app.use(bodyParser.urlencoded({
-            extended: true
-        }));
-
-        //mount cookie parser middleware
-        this.app.use(cookieParser("SECRET_GOES_HERE"));
-
-        // catch 404 and forward to error handler
-        this.app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
-            err.status = 404;
-            next(err);
-        });
-
-        //error handling
-        this.app.use(errorHandler());
-    }
-
-    /**
-     * Create and return Router.
-     *
-     * @class Server
-     * @method routes
-     * @return void
-     */
-    private routes() {
-        let router: express.Router;
-        router = express.Router();
-        IndexRoute.create(router);
-        LoginRoute.create(router);
-        SignUpRoute.create(router);
-        ProfileRoute.create(router);
-        //use router middleware
-        this.app.use(router);
-    }
+  /**
+   * Create and return Router.
+   *
+   * @class Server
+   * @method routes
+   * @return void
+   */
+  private routes() {
+    let router: express.Router;
+    router = express.Router();
+    IndexRoute.create(router);
+    LoginRoute.create(router);
+    SignUpRoute.create(router);
+    ProfileRoute.create(router);
+    //use router middleware
+    this.app.use(router);
+  }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,54 +1,50 @@
-import { client } from '../index'
+import { client } from "../index";
 
 export interface IUser {
-    firstName: string
-    lastName: string
-    email: string
-    username: string
-    profilePic: string
-    bio: string
-    location: string
-    isAdmin: boolean
+  _id?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  profilePic: string;
+  bio: string;
+  location: string;
+  joined: string;
+  reviews: string[];
+  isAdmin: boolean;
 }
 
 export class User {
-
-    public static async getUser(username: string){
-        const collection = client.db('runaway').collection('users')
-        const user = await collection.find({'username': username}).toArray()
-        if(username==="wallace") {
-            return Promise.resolve({
-            firstName: "Wallace",
-            lastName: "Berder",
-            email: "wberder@gmail.com",
-            username: "wallace",
-            profilePic: "https://pbs.twimg.com/profile_images/1163541386785746944/A1nz8DcJ_400x400.jpg",
-            bio: "My name is Wallace Berder, and I enjoy touring around Europe",
-            location: "Moscow, Russia",
-            joined: "2019-09-01",
-            isAdmin: true,
-            reviewCount: 0,
-            isOwnProfile: true
-        })
-        } else {
-            return Promise.reject("Username not found")
-        }
+  public static async getUser(username: string) {
+    const collection = client.db("runaway").collection("users");
+    const user = await collection.find({ username: username }).toArray();
+    if (user.length == 0) {
+      return Promise.reject(`No users found matching username ${username}`);
+    } else if (user.length == 1) {
+      console.log(user[0]);
+      return user[0] as IUser;
+    } else {
+      return Promise.reject(new Error("More than one user found."));
     }
+  }
 
-    public static async getUsers() {
+  public static async getUsers() {
+    const collection = client.db("runaway").collection("users");
+    const users = await collection.find({}).toArray();
+    return users;
+  }
 
-    }
+  public static async deleteUser(user: IUser) {
+    const collection = client.db("runaway").collection("users");
+    const result = await collection.deleteOne(user);
+    console.log(result);
+  }
 
-    public static async deleteUser() {
+  public static async updateUser() {}
 
-    }
-
-    public static async updateUser() {
-
-    }
-
-    public static async addUser() {
-
-    }
-
+  public static async addUser(user: IUser) {
+    const collection = client.db("runaway").collection("users");
+    const result = await collection.insertOne(user);
+    console.log(result);
+  }
 }
